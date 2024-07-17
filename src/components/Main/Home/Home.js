f90abb9039f251bbff3e1b418bc176d6c9a6d740
Bohdan Grachov <bgrachov.94@gmail.com>
2024-07-17 10:33:57 +0300

    lazy loading test

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -1,16 +1,36 @@
-import React from "react";
-import HomeTitle from "./HomeTitle/HomeTitle";
-import HomeSlider from "./HomeSlider/HomeSlider";
-import HomeCoaches from "./HomeCoaches/HomeCoaches";
-import HomePartners from "./HomePartners/HomePartners";
+import React, { Suspense, useEffect } from 'react';
+
+// Preload Components
+const preloadComponent = (component) => component.preload && component.preload();
+
+// Lazy load components
+const HomeTitle = React.lazy(() => import('./HomeTitle/HomeTitle'));
+const HomeSlider = React.lazy(() => import('./HomeSlider/HomeSlider'));
+const HomeCoaches = React.lazy(() => import('./HomeCoaches/HomeCoaches'));
+const HomePartners = React.lazy(() => import('./HomePartners/HomePartners'));
 
 function Home() {
+  useEffect(() => {
+    // Preload components when Home component mounts
+    preloadComponent(HomeSlider);
+    preloadComponent(HomeCoaches);
+    preloadComponent(HomePartners);
+  }, []);
+
   return (
     <main>
-      <HomeTitle />
-      <HomeSlider />
-      <HomeCoaches />
-      <HomePartners />
+      <Suspense fallback={<div>Loading title...</div>}>
+        <HomeTitle />
+      </Suspense>
+      <Suspense fallback={<div>Loading slider...</div>}>
+        <HomeSlider />
+      </Suspense>
+      <Suspense fallback={<div>Loading coaches...</div>}>
+        <HomeCoaches />
+      </Suspense>
+      <Suspense fallback={<div>Loading partners...</div>}>
+        <HomePartners />
+      </Suspense>
     </main>
   );
 }
