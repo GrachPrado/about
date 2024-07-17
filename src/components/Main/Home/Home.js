59d449f7ba2bb0ce4d0dfbc3e66d9a3ebf25c3e2
Bohdan Grachov <bgrachov.94@gmail.com>
2024-07-17 10:07:44 +0300

    lazy loading test

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -1,20 +1,28 @@
 import React,{ Suspense } from 'react';
-import HomeTitle from './HomeTitle/HomeTitle';
-import HomeSlider from './HomeSlider/HomeSlider';
-import HomeCoaches from './HomeCoaches/HomeCoaches';
-import HomePartners from './HomePartners/HomePartners';
+import { lazy } from 'react';
+
+const HomeTitle = lazy(() => import('./HomeTitle/HomeTitle'));
+const HomeSlider = lazy(() => import('./HomeSlider/HomeSlider'));
+const HomeCoaches = lazy(() => import('./HomeCoaches/HomeCoaches'));
+const HomePartners = lazy(() => import('./HomePartners/HomePartners'));
 
 function Home() {
-  return(
+  return (
     <main>
-    <Suspense fallback={<div>Loading...</div>}>
-    <HomeTitle />
-    </Suspense>
-    <HomeSlider />
-    <HomeCoaches/>
-    <HomePartners />
+      <Suspense fallback={<div>Loading...</div>}>
+        <HomeTitle />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <HomeSlider />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <HomeCoaches />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <HomePartners />
+      </Suspense>
     </main>
-)
+  );
 }
 
 export default Home;
